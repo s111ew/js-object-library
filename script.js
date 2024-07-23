138b770d0840cd378f0c6d354db6479998dbf374
s111ew <167631312+s111ew@users.noreply.github.com>
2024-07-23 19:46:12 +0100

    Add library array and new book constructor

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function initialiseButtons() {
             closePopUp();
 }});
 const submitButton = document.querySelector(".submit-button");
-submitButton.addEventListener("click", addBook)
+submitButton.addEventListener("click", addBooktoLibrary)
 }
 
 function removeButtonInitialise(buttonSelector, windowSelector, targetSelector, newDivClass) {
@@ -59,6 +59,60 @@ function removeButtonInitialise(buttonSelector, windowSelector, targetSelector, 
         }
         });
     });
+}
+
+let library = [
+    {
+        title: "No Country for Old Men",
+        author: "Cormac McCarthy",
+        genre: "Thriller",
+        year: "2005",
+        pages: "320",
+        read: "on"
+    },
+    {
+        title: "Kiss Me, Judas",
+        author: "Christpher Baer",
+        genre: "Crime",
+        year: "1998",
+        pages: "316",
+        read: "on"
+    }
+]
+
+function Book(title, author, genre, year, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+    this.year = year;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBooktoLibrary() {
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const genre = document.querySelector("#genre").value;
+    const year = document.querySelector("#year").value;
+    const pages = document.querySelector("#pages").value;
+    const read = document.querySelector("#read").value;
+
+    function closePopUp() {
+        const popUps = document.querySelectorAll(".pop-up");
+        popUps.forEach(popUp => {
+            popUp.classList.add("not-visible");
+        });
+        const inputs = document.querySelectorAll("input:not(.button)");
+        inputs.forEach(input => {
+            input.value = "";
+        });
+    }
+
+    if (title !== "" && author !== "" && genre !== "") {
+        let bookToAdd = new Book(title, author, genre, year, pages, read);
+        library.push(bookToAdd);
+        closePopUp();
+        }
 }
 
 removeButtonInitialise(".remove", ".card-container", ".add-book", "card-container");
